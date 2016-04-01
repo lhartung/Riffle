ae5490bff9941b1b941330cdd17ec3314bf8c574
Lance Hartung <lance@exis.io>
2016-04-01 11:47:36 -0500

    Fixed mantle?

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Riffle",
     dependencies: [
-        .Package(url: "Packages/Mantle", majorVersion: 0)
+        .Package(url: "Packages/", majorVersion: 0)
     ]
 )
