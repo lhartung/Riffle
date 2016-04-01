816991cb95bab0eb3a8e8c7160138705360a640d
Lance Hartung <lance@exis.io>
2016-04-01 12:03:48 -0500

    Fixed syntax error.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Riffle",
     dependencies: [
         .Package(url: "", majorVersion: 0)
-    ]
+    ],
     targets: [
         Target(
             name: "Riffle",
